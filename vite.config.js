56db97b4d23d073636554649512c83bf6143ac70
Abhilash Asolkar <abhilash.asolkar@iiml.org>
2026-05-19 16:27:37 +0530

    chore: add gh-pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/CRM-capabilities-in-Kapture/',
   plugins: [react(), tailwindcss()],
 })
